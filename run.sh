5df5d7af1793a5d53ba936662e8bab2dd5c58fec
Daniil Bolotov <bolotov.di@esphere.ru>
2021-05-22 22:21:45 +0300

    done with performance analysis for Treiber stack.

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
 python3.4 run.py run mcs
-python3.4 run.py run clh
+python3.4 run.py data mcs theoretical_throughput throughput
+python3.4 run.py run treiber
+python3.4 run.py data treiber theoretical_throughput throughput
